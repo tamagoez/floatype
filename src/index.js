7ce30b3b00bb337963c1e3873352ac6babe1d469
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 00:48:12 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <p class="font-mono font-bold type-2xl text-center">FloaType</p>,
+  title,
   document.getElementById('root')
 );
 
@@ -13,3 +13,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const title = (
+  <p class="font-mono font-bold type-2xl text-center">FloaType</p>,
+  )
