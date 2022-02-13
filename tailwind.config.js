14230296fb21185704ab536ce6097d775d400f6c
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 11:30:21 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,6 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      sans: ['Work Sans', ...fontFamily.sans],
-    },
-    extend: {},
-  },
   variants: {
     extend: {},
   },
